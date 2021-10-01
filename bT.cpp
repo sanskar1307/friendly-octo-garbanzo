6ce609a90279350a16b85543fcd94310ee778179
sanskarkabra0 <72148180+sanskarkabra0@users.noreply.github.com>
2021-10-01 13:13:28 +0530

    fourth

---
+++ bT.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *left=NULL,*right=NULL;
+
+    Node(int data){
+        data = data;
+    }
+
+}
+
+int main(){
+      Node *root = new Node(1);
+      
+}
